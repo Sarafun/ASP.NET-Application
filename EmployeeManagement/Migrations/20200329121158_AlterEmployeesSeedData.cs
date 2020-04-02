95da3542f16340036610db867097b7ca9a9949eb
Sarafun <hrynevichvolodymyr@gmail.com>
2020-04-02 12:10:14 +0300

    Ready to push

---
+++ EmployeeManagement/Migrations/20200329121158_AlterEmployeesSeedData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AlterEmployeesSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Department", "Email", "Name" },
+                values: new object[] { 1, "mery@meta.ua", "Mery" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 2, "john@meta.ua", "John" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 3, 2, "mark@meta.ua", "Mark" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Department", "Email", "Name" },
+                values: new object[] { 2, "mark@meta.ua", "Mark" });
+        }
+    }
+}
---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<AppDbContext>
+                (options => options.UseSqlServer(_config.GetConnectionString
+                ("EmployeeDBConnection")));
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                //(options =>
+                //{
+                //    options.Password.RequiredLength = 4;
+                //    options.Password.RequiredUniqueChars = 2;
+                //    options.SingIn.RequireConfirmedEmail = true;
+                //})
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddMvc(options=>{
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            }).AddXmlSerializerFormatters();
+            services.AddAuthentication().AddGoogle(options =>
+            {
+                options.ClientId = "11111";
+                options.ClientSecret = "SomeClient";
+            })
+                .AddFacebook(options =>
+                {
+                    options.AppId = "11111";
+                    options.AppSecret = "SomeSecret";
+                });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("DeleteRolePolicy",
+                    policy => policy.RequireClaim("Delete Role"));
+                options.AddPolicy("EditRolePolicy",
+                    policy => policy.RequireClaim("Edit Role"));
+                options.AddPolicy("AdminRolePolicy",
+                    policy => policy.RequireRole("Admin"));
+            });
+            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            }
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
